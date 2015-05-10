ff53e0268efa5d3c53885d2131d1e3f5a589a33e
Scott Lystig Fritchie <slfritchie@snookles.com>
2015-05-11 00:07:11 +0900

    Add README.sh

---
+++ README.sh
@@ -0,0 +1,6 @@
+
+## The SLF configurator
+
+Because I'm lazy, old, set-in-my-ways, why-are-chef-and-ansible such
+absurdly big hammers.  Time to be a big dumb, for the greater good.
+Or, at least, time to be lazy.
