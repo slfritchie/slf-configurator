8e77201ae6e9b9311353bdccf1a3b1548a715343
Scott Lystig Fritchie <slfritchie@snookles.com>
2017-10-16 18:46:50 -0500

    Add libssl-dev to ponylang-ponyc.sh

--- ponylang-ponyc.sh
+++ ponylang-ponyc.sh
@@ -18,7 +18,7 @@ case $OS_DISTRO in
         # Sendence's ponyc & Wallaroo requires gcc 5 for its atomics support
 	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
         sudo apt-get update
-        sudo apt-get install -y gcc-5 g++-5
+        sudo apt-get install -y gcc-5 g++-5 libssl-dev
         sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 
         # Installing ponyc
