e1502bba33d9ff93dc9b875bcfb084277db53628
Scott Lystig Fritchie <slfritchie@snookles.com>
2017-10-16 14:19:20 -0500

    Add os-install-docker.sh

--- ponylang-ponyc.sh
+++ ponylang-ponyc.sh
@@ -64,29 +64,6 @@ EOF
 
         ## Install Python Development Libraries
         sudo apt-get install -y python-dev
-
-        ## Install Docker
-        ## We will use the CE (Community Edition)
-        ## https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#recommended-extra-packages-for-trusty-1404
-        ## https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#install-using-the-repository
-        case $DISTRIB_CODENAME in
-            trusty)
-                sudo apt-get install -y \
-                     linux-image-extra-$(uname -r) \
-                     linux-image-extra-virtual
-                ;;
-        esac
-        sudo apt-get install \
-             apt-transport-https \
-             ca-certificates \
-             curl \
-             software-properties-common
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-        sudo add-apt-repository \
-             "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-                $(lsb_release -cs) \
-                   stable"
-        sudo apt-get install docker-ce
     ;;
     centos)
         echo "TODO, aborting!"
