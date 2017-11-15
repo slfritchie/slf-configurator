e2abab96f2e060a88c61b5bb38d9914d76310e9b
Scott Lystig Fritchie <slfritchie@snookles.com>
2017-11-15 17:07:31 -0600

    Add commented-out recipe to download & build LLVM 3.7.1 for CentOS 7

--- os-install-devel.sh
+++ os-install-devel.sh
@@ -63,6 +63,63 @@ EOF
         yum install -y \
 	    make gcc gcc-c++ kernel-devel m4 \
             git expect patch
+
+        echo 'Installing LLVM 3.7.1 or 3.8.1 or 3.9.1 for use'
+        echo 'with Pony development is *extremely* difficult on CentOS 7.'
+        echo "SKIPPING installing any LLVM."
+
+# NOTE: This recipe will work, but it takes *hours* to compile on a
+# modest virtual machine.....
+#
+#        mkdir /root/llvm
+#        (
+#            cd /root/llvm
+#            wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+#            rpm -ivh epel-release-latest-7.noarch.rpm
+#            yum install -y \
+#                llvm3.9 llvm3.9-devel llvm3.9-libs llvm3.9-static
+#
+#            echo BAH, the above does not work either
+#            ### http://www.linuxfromscratch.org/blfs/view/7.9/general/llvm.html
+#
+#            yum install -y \
+#                libffi libffi-devel
+#            wget http://llvm.org/releases/3.7.1/llvm-3.7.1.src.tar.xz
+#            wget http://llvm.org/releases/3.7.1/cfe-3.7.1.src.tar.xz
+#            wget http://llvm.org/releases/3.7.1/compiler-rt-3.7.1.src.tar.xz
+#            unxz -c < llvm-3.7.1.src.tar.xz | tar xf -
+#            (
+#                cd llvm-3.7.1.src
+#                tar -xf ../cfe-3.7.1.src.tar.xz -C tools &&
+#                    tar -xf ../compiler-rt-3.7.1.src.tar.xz -C projects &&
+#                    mv tools/cfe-3.7.1.src tools/clang &&
+#                    mv projects/compiler-rt-3.7.1.src projects/compiler-rt
+#                sed -r "/ifeq.*CompilerTargetArch/s#i386#i686#g" \
+#                    -i projects/compiler-rt/make/platform/clang_linux.mk
+#
+#                sed -e "s:/docs/llvm:/share/doc/llvm-3.7.1:" \
+#                    -i Makefile.config.in &&
+#                    mkdir -v build &&
+#                    cd       build &&
+#                    CC=gcc CXX=g++                          \
+#                      ../configure --prefix=/usr/local      \
+#                      --datarootdir=/usr/local/share   \
+#                      --sysconfdir=/etc          \
+#                      --enable-libffi            \
+#                      --enable-optimized         \
+#                      --enable-shared            \
+#                      --enable-targets=host,r600 \
+#                      --disable-assertions       \
+#                      --docdir=/usr/local/share/doc/llvm-3.7.1 &&
+#                    make
+#                make install &&
+#                    for file in /usr/local/lib/lib{clang,LLVM,LTO}*.a
+#                    do
+#                        test -f $file && chmod -v 644 $file
+#                    done
+#                unset file
+#            )
+#        )
     ;;
     freebsd)
         ## WEIRD: 'pkg' install of gcc doesn't have 'gcc' executable anywhere?
